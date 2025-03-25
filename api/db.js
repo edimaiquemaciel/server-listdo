1a8bbc783b0acf8c2d619884801a7f0ca1bf2542
Edimaique <edimaiqueacacio@gmail.com>
2025-03-25 19:56:24 -0300

    first commit

---
+++ api/db.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // Lê o db.json da raiz
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// Exporta para o Vercel
+module.exports = server;
